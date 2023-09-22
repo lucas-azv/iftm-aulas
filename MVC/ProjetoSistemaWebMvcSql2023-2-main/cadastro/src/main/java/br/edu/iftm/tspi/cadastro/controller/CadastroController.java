e21c4f0663b4a4364ed8751de6db24363e4d6161
Lucas <108193174+lucas-azv@users.noreply.github.com>
2023-09-22 15:00:29 -0300

    Add files via upload

--- MVC/ProjetoSistemaWebMvcSql2023-2-main/cadastro/src/main/java/br/edu/iftm/tspi/cadastro/controller/CadastroController.java
+++ MVC/ProjetoSistemaWebMvcSql2023-2-main/cadastro/src/main/java/br/edu/iftm/tspi/cadastro/controller/CadastroController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,18 +18,67 @@ public class CadastroController {
     List<CadastroDTO> cadastros = new ArrayList<>();
 
     @PostMapping("cadastroResourcePost")
-    public String doPost(CadastroDTO dto,Model model) {
-        dto.id = cadastros.size() + 1L;
+    public String doPost(CadastroDTO dto, Model model) {
+
+        dto.setId((long) (cadastros.size() + 1));
         cadastros.add(dto);
-        //model.addAttribute("cadastros",cadastros);
-        //return "listagem";
         return doGet(model);
+
     }
 
     @RequestMapping("cadastroResourceGet")
     public String doGet(Model model) {
-        model.addAttribute("cadastros",cadastros);
+        model.addAttribute("cadastros", cadastros);
         return "listagem";
     }
-    
+
+    @GetMapping("/edit/{id}")
+public String edit(@PathVariable("id") Long id, Model model) {
+    CadastroDTO cadastroParaEditar = encontrarCadastroPorId(id);
+
+    if (cadastroParaEditar != null) {
+        model.addAttribute("cadastro", cadastroParaEditar);
+
+        return "editar";
+    } else {
+        return "redirect:/cadastroResourceGet";
+    }
 }
+
+@PostMapping("/editarCadastro")
+public String atualizarCadastro(CadastroDTO dto, Model model) {
+    CadastroDTO cadastroOriginal = encontrarCadastroPorId(dto.getId());
+
+    if (cadastroOriginal != null) {
+        cadastroOriginal.setInputNome(dto.getInputNome());
+        cadastroOriginal.setInputEmail(dto.getInputEmail());
+        cadastroOriginal.setInputTelefone(dto.getInputTelefone());
+
+        return "redirect:/cadastroResourceGet";
+    } else {
+        return "redirect:/cadastroResourceGet";
+    }
+}
+
+
+    @GetMapping("/excluirCadastro/{id}")
+    public String excluirCadastro(@PathVariable("id") Long id) {
+        CadastroDTO cadastro = encontrarCadastroPorId(id);
+
+        if (cadastro != null) {
+            cadastros.remove(cadastro);
+        }
+
+        return "redirect:/cadastroResourceGet";
+    }
+
+    private CadastroDTO encontrarCadastroPorId(Long id) {
+        for (CadastroDTO cadastro : cadastros) {
+            if (cadastro.getId().equals(id)) {
+                return cadastro;
+            }
+        }
+        return null;
+    }
+}
+
--- MVC/ProjetoSistemaWebMvcSql2023-2-main/cadastro/src/main/java/br/edu/iftm/tspi/cadastro/dto/CadastroDTO.java
+++ MVC/ProjetoSistemaWebMvcSql2023-2-main/cadastro/src/main/java/br/edu/iftm/tspi/cadastro/dto/CadastroDTO.java
@@ -5,9 +5,46 @@ import lombok.Data;
 @Data
 public class CadastroDTO {
 
-    public long id;
+    public CadastroDTO(Long id, String inputNome, String inputEmail, String inputTelefone){
+        this.id = id;
+        this.inputNome = inputNome;
+        this.inputEmail = inputEmail;
+        this.inputTelefone = inputTelefone;
+    }
+
+    public CadastroDTO(){
+    }
+
+    public Long id;
     private String inputNome;
     private String inputEmail;
     private String inputTelefone;
-    
+
+    public void setId(Long id){
+        this.id = id;
+    }
+    public Long getId(){
+        return id;
+    }
+
+    public void setNome(String inputNome){
+        this.inputNome = inputNome;
+    }
+    public String getNome(){
+        return inputNome;
+    }
+
+    public void setEmail(String inputEmail){
+        this.inputEmail = inputEmail;
+    }
+    public String getEmail(){
+        return inputEmail;
+    }
+
+    public void setTelefone(String inputTelefone){
+        this.inputTelefone = inputTelefone;
+    }
+    public String getTelefone(){
+        return inputTelefone;
+    }
 }
