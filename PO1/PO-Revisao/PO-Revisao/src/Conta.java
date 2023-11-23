853ceab9994af3e73163b034468005684581fbd7
Lucas <108193174+lucas-azv@users.noreply.github.com>
2023-11-23 19:17:52 -0300

    Update Conta.java

--- PO1/PO-Revisao/PO-Revisao/src/Conta.java
+++ PO1/PO-Revisao/PO-Revisao/src/Conta.java
@@ -2,16 +2,16 @@ public class Conta {
     private int numero;
     private Cliente correntista;
     private float saldo;
-    
+
+    public Conta() {
+    }
+
     public Conta(int numero, Cliente correntista, float saldo) {
         this.numero = numero;
         this.correntista = correntista;
         this.saldo = saldo;
     }
 
-    public Conta(){
-
-    }
     public int getNumero() {
         return numero;
     }
@@ -36,19 +36,30 @@ public class Conta {
         this.saldo = saldo;
     }
 
-    public void depositar(float valorDeposito){
-        saldo += valorDeposito;
+    public void depositar(float valor) {
+        this.saldo += valor;
     }
-    public boolean sacar(float valorSaque){
-        if(valorSaque <= saldo){
-            saldo -= valorSaque;
+
+    public void sacar(float valor) throws Restricoes {
+        if (valor > saldo) {
+            throw new Restricoes("Saque com saldo insuficiente");
+        }
+        this.saldo -= valor;
+    }
+
+    public boolean movimentar(float valor, int operacao) {
+        try {
+            if (operacao == Movimento.SACAR) {
+                sacar(valor);
+            } else if (operacao == Movimento.DEPOSITAR) {
+                depositar(valor);
+            } else {
+                return false; // Operação inválida
+            }
             return true;
-        } else{
+        } catch (Restricoes e) {
+            System.out.println(e.getMessage());
             return false;
         }
-    }
-    public boolean movimentar(float saldo, int numero){
-
-        return false;
     }
 }
