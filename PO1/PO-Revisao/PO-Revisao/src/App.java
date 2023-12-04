db9e389e82cea36e812a4b0385c2901fad475b58
Lucas <108193174+lucas-azv@users.noreply.github.com>
2023-12-03 22:24:20 -0300

    Update App.java

--- PO1/PO-Revisao/PO-Revisao/src/App.java
+++ PO1/PO-Revisao/PO-Revisao/src/App.java
@@ -1,36 +1,55 @@
-import javax.swing.JOptionPane;
+import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 public class App {
+
     public static void main(String[] args) {
-        Cliente cliente = new Cliente("Nome Cliente", "123.456.789-01");
-        Conta conta = new Conta(1, cliente, 1000.0);
 
-        Transacao transacao = new Transacao();
-
-        try {
-            float saldoAnterior = conta.getSaldo();
-
-            transacao.realizarTransacao("2023-11-23", conta, 500.0, "Compra", Movimento.SACAR);
-            transacao.realizarTransacao("2023-11-24", conta, 200.0, "Depósito", Movimento.DEPOSITAR);
-
-            List<Movimento> movimentos = transacao.getMovimentos();
-
-            String relatorio = "Relatório:\n" +
-                               "Saldo anterior: " + saldoAnterior + "\n\n";
-
-            for (Movimento movimento : movimentos) {
-                relatorio += "Data: " + movimento.getData() + "\n" +
-                             "Histórico: " + movimento.getHistorico() + "\n" +
-                             "Valor: " + movimento.getValor() + "\n" +
-                             "Saldo Anterior: " + movimento.getSaldoAnterior() + "\n" +
-                             "Saldo Atual: " + movimento.getConta().getSaldo() + "\n\n";
-            }
-
-            JOptionPane.showMessageDialog(null, relatorio, "Relatório", JOptionPane.INFORMATION_MESSAGE);
-
-        } catch (Restricoes e) {
-            JOptionPane.showMessageDialog(null, e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+        String usuario, senha;
+        char resp;
+        boolean flag = true;
+        
+        List<String> validacao = new ArrayList<>();
+        validacao.add("fulano:123");
+        validacao.add("beltrano:456");
+        validacao.add("ciclano:789");
+        
+        Transacao t = new Transacao();
+        Cliente cl = new Cliente("Clarimundo","99999999999");
+        Conta co = new Conta(123456,cl,1000.0f);
+        float saldoAnterior = co.getSaldo();       
+        
+        t.realizarTransacao("02/11/2023", co, "Depósito Dinheiro", 300.0f, Movimento.DEPOSITAR);                
+        t.realizarTransacao("02/11/2023", co, "Pagamento Conta de Luz", 5000.0f, Movimento.SACAR);                
+        t.realizarTransacao("03/11/2023", co, "Pagamento Conta de Telefone", 120.0f, Movimento.SACAR);                
+        t.realizarTransacao("10/11/2023", co, "Recebimento de PIX", 500.0f, Movimento.DEPOSITAR);   
+        try{        
+            resp = JOptionPane.showInputDialog("Deseja emitir relatório? (s/n)").charAt(0);
+            if(resp=='s'||resp=='S'){
+                usuario = JOptionPane.showInputDialog("Entre com o usuário");
+                senha = JOptionPane.showInputDialog("Entre com a senha");
+                for(String s : validacao)
+                    if(s.compareTo(usuario+":"+senha)==0){
+                        flag = false;    
+                        String texto = "Emitindo Extrato da Conta de Número: "+co.getNumero()+
+                                "\n\nCorrentista: "+cl.getNome()+"\nCPF: "+cl.getCpf()+
+                                "\nSaldo Anterior: "+saldoAnterior+"\n\n";
+                        for(Movimento mov : t.getMovimentos())
+                          texto += "........................................\nData: "+ mov.getData()+
+                                   "\nHistórico: "+ mov.getHistorico()+"\nValor: "+mov.getValor()+
+                                   "\nOperação: "+(mov.getOperacao()==Movimento.DEPOSITAR?"Depósito":"Saque")+"\n";
+                        texto+="\nSaldo Atual: "+co.getSaldo();
+                        JOptionPane.showMessageDialog(null,texto,"RELATÓRIO",1);
+                        break;
+                    }
+                if(flag)    
+                    throw new Restricoes("Usuário não autorizado a ver o relatório");
+            }                                                 
         }
+        catch(Restricoes r){
+                JOptionPane.showMessageDialog(null,r.toString(),"RESTRIÇÃO DE IDENTIFICAÇÃO",1);              
+        } 
     }
+    
 }
