264aeb3225c71605eeed701e4d8fa56cd5369843
Lucas <108193174+lucas-azv@users.noreply.github.com>
2023-12-03 22:25:27 -0300

    Update Conta.java

--- PO1/PO-Revisao/PO-Revisao/src/Conta.java
+++ PO1/PO-Revisao/PO-Revisao/src/Conta.java
@@ -1,12 +1,14 @@
+import javax.swing.JOptionPane;
+
 public class Conta {
     private int numero;
     private Cliente correntista;
     private float saldo;
-
-    public Conta() {
+    
+    public Conta(){
     }
-
-    public Conta(int numero, Cliente correntista, float saldo) {
+    
+    public Conta(int numero, Cliente correntista, float saldo){
         this.numero = numero;
         this.correntista = correntista;
         this.saldo = saldo;
@@ -35,31 +37,32 @@ public class Conta {
     public void setSaldo(float saldo) {
         this.saldo = saldo;
     }
-
-    public void depositar(float valor) {
-        this.saldo += valor;
+    
+    public void depositar(float valor){
+        this.setSaldo(this.getSaldo() + valor);
     }
-
-    public void sacar(float valor) throws Restricoes {
-        if (valor > saldo) {
-            throw new Restricoes("Saque com saldo insuficiente");
-        }
-        this.saldo -= valor;
-    }
-
-    public boolean movimentar(float valor, int operacao) {
-        try {
-            if (operacao == Movimento.SACAR) {
-                sacar(valor);
-            } else if (operacao == Movimento.DEPOSITAR) {
-                depositar(valor);
-            } else {
-                return false; // Operação inválida
+    
+    public boolean sacar(float valor){
+        try{
+            if(valor <= this.getSaldo()){     
+                float x = this.getSaldo() - valor;
+                this.setSaldo(x);
+                return true;
             }
-            return true;
-        } catch (Restricoes e) {
-            System.out.println(e.getMessage());
-            return false;
+            else
+                throw new Restricoes("Saldo Insuficiente para operação de SAQUE");                
+        } catch (Restricoes r){
+            JOptionPane.showMessageDialog(null,r.toString(),"RESTRIÇÃO DE SALDO",1);              
         }
+        return false;
     }
+    
+    public boolean movimentar(float valor, int operacao) {
+        if(operacao == 0)
+            return this.sacar(valor);
+        else
+            this.depositar(valor);
+        return true;
+    }       
+    
 }
