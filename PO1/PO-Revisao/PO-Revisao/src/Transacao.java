d1d6f9907b2a949a3897fc0e3a325a760d4b1db3
Lucas <108193174+lucas-azv@users.noreply.github.com>
2023-12-03 22:26:44 -0300

    Update Transacao.java

--- PO1/PO-Revisao/PO-Revisao/src/Transacao.java
+++ PO1/PO-Revisao/PO-Revisao/src/Transacao.java
@@ -2,26 +2,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Transacao {
+    
     private List<Movimento> movimentos;
-
-    public Transacao() {
+    
+    public Transacao(){      
         this.movimentos = new ArrayList<>();
     }
-
-    public boolean realizarTransacao(String data, Conta conta, float valor, String historico, int operacao) {
-        Movimento movimento = new Movimento(data, conta, historico, valor, operacao);
-        if (movimento.movimentar()) {
-            movimentos.add(movimento);
-            return true;
-        }
-        return false;
+    
+    public boolean realizarTransacao(String data, Conta conta, String historico, float valor, int operacao){
+        Movimento mov = new Movimento(data,conta,historico,valor,operacao);
+        if(!mov.movimentar())
+            return false;
+        this.movimentos.add(mov);
+        return true;
     }
-
-    public void estornarTransacao() {
-        movimentos = new ArrayList<>();
+    
+    public void estornarTransacao(){
+        for(Movimento m : this.movimentos)
+            m = null;
     }
-
-    public List<Movimento> getMovimentos() {
-        return movimentos;
+    
+    public List<Movimento> getMovimentos(){
+        return this.movimentos;
     }
 }
